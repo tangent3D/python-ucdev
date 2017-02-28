df887f0be8591899d4098a45c4a6c16404c08bdc
Paul <pc235603@gmail.com>
2017-02-28 22:04:03 +0000

    Corrected setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup 
 setup(name='ucdev',
       version='0.0.1',
-      description='Library to access the GPIO pins of the Cypress CY7C65213',
+      description='Library to access the GPIO pins of the Cypress CY7C65213 in UART mode',
       author='PaulZC',
       author_email='pc235603.remove-if-not-spam@gmail.com',
       license='Python',
-      packages='cy7c65213')
+      packages=['cy7c65213'])
