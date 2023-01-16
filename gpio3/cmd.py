f8d6926f7110ba3072d4f5584550ee59c11297c4
tangent3D <interfectum@gmail.com>
2023-01-16 00:47:13 -0800

    Revert "point to DLLs in repo"
    This reverts commit 5d601c44ee40f0b391cb98b8dffdb454fb91baff.

--- gpio3/cmd.py
+++ gpio3/cmd.py
@@ -30,10 +30,9 @@ def main(argv=None):
 
     # Point to the DLL provided by Cypress
     if (platform.system() == "Windows"):
-        dll = "__file__\\..\\cypress\\win64\\cyusbserial.dll"
-        print(dll)
+        dll = "C:\\Program Files (x86)\\Cypress\\USB-Serial SDK\\library\\cyusbserial\\x64\\cyusbserial.dll"
     if (platform.system() == "Linux"):
-        dll = "__file__/../cypress/linux/libcyusbserial.so"
+        dll = "/usr/local/lib/libcyusbserial.so"
 
     if path.exists(dll):
         # Load the DLL
